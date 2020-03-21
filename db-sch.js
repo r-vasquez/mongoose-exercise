0dce97e11b5b0940a9c3252458446fd278e5b863
jaime <jaime.molina@mattersupply.co>
2020-03-20 20:22:52 -0500

    DB validation insertion
    Co-authored-by: Alejandro Ruiz Sánchez <alejoruizs@hotmail.com>
    Co-authored-by: Rogger Vasquez <rvasque3@gmail.com>
    Co-authored-by: juanesvelez <juanesvelez@gmail.com>
    Co-authored-by: Daniel Burgos Castrillón <dburgos3@eafit.edu.co>
    Co-authored-by: Mateo Muñoz Bustamante <mateomunozb@gmail.com>

--- db-sch.js
+++ db-sch.js
@@ -8,4 +8,21 @@ const movieSchema = new mongoose.Schema({
   id: String,
 })
 
+class Movie {
+  static async findAndInsertMany(moviesJSON) {
+    const movies = await this.find({})
+    const ids = movies.map((movie) => {
+      return movie.id
+    })
+
+    const filterMovies = moviesJSON.filter((movie) => {
+      return !ids.some((ID) => ID === movie.id)
+    })
+
+    return this.insertMany(filterMovies)
+  }
+}
+
+movieSchema.loadClass(Movie)
+
 module.exports = movieSchema
